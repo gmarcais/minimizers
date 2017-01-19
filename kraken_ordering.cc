41d924e642ca6c9a217aa1e86cd346dc772a8fc8
Guillaume Marçais <gmarcais@cs.cmu.edu>
2017-01-19 14:25:27 -0500

    Kraken ordering

---
+++ kraken_ordering.cc
@@ -0,0 +1,21 @@
+#include <random>
+#include <iostream>
+
+#include "misc.hpp"
+#include "kraken_ordering.hpp"
+
+int main(int argc, char *argv[]) {
+  kraken_ordering args(argc, argv);
+  
+  std::mt19937_64 rng;
+  seed_prg(rng);
+
+  const uint64_t mask    = Mask<2>::bases(args.k_arg);
+  const uint64_t nb_mers = mask + 1;
+  const uint64_t rnd = rng() & mask;
+
+  for(uint64_t m = 0; m < nb_mers; ++m)
+    std::cout << (m ^ rnd) << '\n';
+
+  return 0;
+}
